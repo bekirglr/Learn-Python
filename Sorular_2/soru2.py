da0488901b21f2c879ab7445f242825e079491d0
bekirglr <bbekirglr@gmail.com>
2022-11-11 11:33:32 +0300

    en büyük ve en küçük sayıyı bulma

---
+++ Sorular_2/soru2.py
@@ -0,0 +1,16 @@
+sifirMi=True
+eb=0
+ek=0
+while sifirMi:
+    
+    sayi=int(input("Bir sayı girin"))
+    if sayi==0:
+        break
+    else:
+        if sayi>eb:
+            eb=sayi
+        elif sayi<ek:
+            ek=sayi
+            
+print("En büyük",eb)
+print("En küçük",ek)
