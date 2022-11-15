ec15a3c416738beb7a29d665f1d9c94fce35257d
Bekir Guler <bbekirglr@gmail.com>
2022-11-15 19:35:29 +0300

    otopark ucret hesaplama

---
+++ Sorular_2/soru7.py
@@ -0,0 +1,10 @@
+kalinan_saat=int(input("Kaldığınız saati girin"))
+toplam_ucret=0
+if kalinan_saat<=1:
+    toplam_ucret=5
+elif (kalinan_saat>1) and (kalinan_saat<=5):
+    toplam_ucret=kalinan_saat*4
+elif kalinan_saat>5:
+    toplam_ucret=kalinan_saat*3
+    
+print("Ödeyeceğiniz miktar =",toplam_ucret)
